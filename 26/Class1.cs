35aee82d1a4f082a99149d81b4cc1f3de88eeb61
Ku4erenko15 <159886914+Ku4erenko15@users.noreply.github.com>
2025-02-28 12:50:11 +0200

    Add files via upload

---
+++ 26/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26
+{
+    internal class Plesen
+    {
+        private int age {  get; set; }
+        int retirementAge {  get; set; }
+         public Plesen()
+        {
+        }
+        public Plesen(int age, int retirementAge)
+        {
+            this.age = age;
+            this.retirementAge = retirementAge;
+        }
+
+         public bool CheckAge()
+        {
+            int retirementAge = 65;
+
+            if (age >= retirementAge)
+            {
+                Console.WriteLine("Уже на пенсии.");
+            }
+            else
+            {
+                int yearsLeft = retirementAge - age;
+                Console.WriteLine($"Осталось {yearsLeft} лет до пенсии.");
+            }
+            return true;
+
+        }
+
+
+
+
+    }
+}
---
+++ 26/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Plesen chel1 = new Plesen(20,52);
+            Console.WriteLine(chel1.CheckAge());
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
